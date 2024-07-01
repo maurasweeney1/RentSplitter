d1fde624e20a6b72eaf436746fbb783364ef2e78
Maura Sweeney <msweeney@zagmail.gonzaga.edu>
2024-07-01 11:51:54 -0700

    added references to database

--- target/classes/rentSplitterDatabase.sql
+++ target/classes/rentSplitterDatabase.sql
@@ -9,33 +9,36 @@ create table user (
 );
 
 create table roommate (
-	FirstName VARCHAR(50) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	FirstName VARCHAR(50) NOT NULL,
     PhoneNo VARCHAR(10) NOT NULL,
     isHome BOOLEAN NOT NULL
 );
 
 create table bill (
-	billName VARCHAR(20) PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	billName VARCHAR(20) NOT NULL,
     company VARCHAR(20) NULL,
     totalCost DOUBLE, 
     dateDueToCompany INT NOT NULL,
     dateDueToPayer INT NOT NULL,
-    payerName VARCHAR(20), -- FIXME idk what to do about that --
-    StatementDate INT NULL
+    payer_id INT,
+    StatementDate INT NULL,
+    FOREIGN KEY (payer_id) REFERENCES Roommate(id)
 );
 
 create table hasRoommates (
 	username VARCHAR(20) NOT NULL,
     password VARCHAR(20) NOT NULL,
-    FirstName VARCHAR(50) NOT NULL,
+    roommateID INT NOT NULL,
     FOREIGN KEY (username, password) REFERENCES user (username, password) ON DELETE CASCADE ON UPDATE NO ACTION,
-    FOREIGN KEY (FirstName) REFERENCES roommate (FirstName) ON DELETE NO ACTION ON UPDATE NO ACTION
+    FOREIGN KEY (roommateID ) REFERENCES roommate (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 create table hasBills (
 	username VARCHAR(20) NOT NULL,
     password VARCHAR(20) NOT NULL,
-	billName VARCHAR(20) NOT NULL,
+	billID INT NOT NULL,
     FOREIGN KEY (username, password) REFERENCES user (username, password) ON DELETE CASCADE ON UPDATE NO ACTION,
-    FOREIGN KEY (billName) REFERENCES bill (billName) ON DELETE NO ACTION ON UPDATE NO ACTION
+    FOREIGN KEY (billID) REFERENCES bill (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
