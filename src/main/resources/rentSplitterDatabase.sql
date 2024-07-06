ecab0d3c46f2a9dfd918b7f77ea3ece63e5e5efc
Maura Sweeney <msweeney@zagmail.gonzaga.edu>
2024-07-06 00:33:18 -0700

    added group table to database

--- src/main/resources/rentSplitterDatabase.sql
+++ src/main/resources/rentSplitterDatabase.sql
@@ -3,16 +3,12 @@ create schema rentSplitterUsers;
 use rentSplitterUsers;
 
 create table user (
-	username VARCHAR(20) NOT NULL,
+	username VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL,
-    PRIMARY KEY (username, password)
-);
-
-create table roommate (
-    id INT AUTO_INCREMENT PRIMARY KEY,
 	FirstName VARCHAR(50) NOT NULL,
     PhoneNo VARCHAR(10) NOT NULL,
-    isHome BOOLEAN NOT NULL
+    isHome BOOLEAN NOT NULL,
+    PRIMARY KEY (username, password)
 );
 
 create table bill (
@@ -24,15 +20,19 @@ create table bill (
     dateDueToPayer INT NOT NULL,
     payer_id INT,
     StatementDate INT NULL,
-    FOREIGN KEY (payer_id) REFERENCES Roommate(id)
+    FOREIGN KEY (payer_id) REFERENCES Roommate (id)
 );
 
-create table hasRoommates (
-	username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    roommateID INT NOT NULL,
-    FOREIGN KEY (username, password) REFERENCES user (username, password) ON DELETE CASCADE ON UPDATE NO ACTION,
-    FOREIGN KEY (roommateID ) REFERENCES roommate (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+create table houseGroup (
+	id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+create table groupMemberships (
+	memberUsername VARCHAR(20) NOT NULL,
+	memberPassword VARCHAR(20) NOT NULL,
+    houseID INT,
+    FOREIGN KEY (memberUsername, memberPassword) REFERENCES user (username, password),
+    FOREIGN KEY (houseID) REFERENCES houseGroup(id)
 );
 
 create table hasBills (
@@ -43,4 +43,4 @@ create table hasBills (
     FOREIGN KEY (billID) REFERENCES bill (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
---  --
+-- if roommate is deleted check if theyre payer and update --
